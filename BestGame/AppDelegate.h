3772441e7616a7fa9fbd7ec395d6e93abe70fe48
student <student@mac-pro-admin-94.local>
2017-07-07 15:05:53 +0300

    Initial Commit

---
+++ BestGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BestGame
+//
+//  Created by student on 07.07.17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BestGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BestGame
+//
+//  Created by student on 07.07.17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
